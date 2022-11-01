563680faa746fde9fd050e6e4b8a972b54bed6e0
da4nick07 <vlevkovsky@mail.ru>
2022-11-01 16:41:41 +0300

    PHPUnit - два теста для MainController.php

--- tests/Controllers/MainControllerTest.php
+++ tests/Controllers/MainControllerTest.php
@@ -2,12 +2,34 @@
 
 namespace Controllers;
 
+use Controllers\MainController;
 use PHPUnit\Framework\TestCase;
 
 class MainControllerTest extends TestCase
 {
+    const PAGING_BLOCK = '!&lt; Пред &gt;</span>&nbsp&nbsp<b>1 </b>&nbsp<a href="/2">&lt; След &gt</a>!';
+    protected $main ;
+    protected $params ;
+
+    protected function setUp(): void
+    {
+        $this->main = new MainController();
+        $this->params[ USER ] = null;
+    }
+
+    protected function tearDown(): void
+    {
+        $this->main = NULL;
+        unset($this->params);
+    }
+
+    /**
+     * @return void
+     * @covers MainController::main
+     */
     public function testMain()
     {
+/*
         // Необязательно: протестируйте здесь что-нибудь, если хотите.
         $this->assertTrue(true, 'This should already work.');
 
@@ -15,5 +37,33 @@ class MainControllerTest extends TestCase
         $this->markTestIncomplete(
             'Этот тест ещё не реализован.'
         );
+*/
+        // если всё ОК - на гл. странице д.б. блок пагинации
+        $this->expectOutputRegex(self::PAGING_BLOCK);
+        $this->params[ MATCHES ] = array( '/1', '1');
+        $this->main->main($this->params);
+
+    }
+
+    /**
+     * @return void
+     * @covers MainController::page
+     * @dataProvider providerPage
+     */
+    public function testPage($matches)
+    {
+        // если всё ОК - на гл. странице д.б. блок пагинации
+        $this->expectOutputRegex(self::PAGING_BLOCK);
+        $this->params[ MATCHES ] = $matches;
+        $this->main->main($this->params);
+
+    }
+
+    public function providerPage ()
+    {
+        return [
+            'first page'  => ['/1', '1'],
+            'secend page' => ['/2', '2']
+        ];
     }
 }
